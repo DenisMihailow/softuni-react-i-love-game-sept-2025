853cede05d94e7d2ceac19a0595019ced41c91c2
DenisMihailow <barakata8@gmail.com>
2025-11-20 03:05:28 +0200

    show detail info static page

--- client/src/components/game/Game.jsx
+++ client/src/components/game/Game.jsx
@@ -11,7 +11,7 @@ export default function Game({
                 <div className="details-overlay">
                     <p className="name">{title}</p>
                     <p className="genre">{genre}</p>
-                    <Link to={`/games${_id}/details`} className="details-button">Details</Link>
+                    <Link to={`/games/${_id}/details`} className="details-button">Details</Link>
                 </div>
             </div>
     );
