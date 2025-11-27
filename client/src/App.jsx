902c99ded5db39805f4b538254bc33ddc5a601aa
DenisMihailow <barakata8@gmail.com>
2025-11-27 22:19:24 +0200

    Create use fetch hook use request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,30 +11,28 @@ import Login from "./components/login/Login"
 import Logout from "./components/logout/Logout"
 import Edit from "./components/edit/Edit"
 import UserContext from "./contexts/UserContext"
+import useRequest from "./hooks/useFetch"
 
 function App() {
   const [user, setUser] = useState(null);
+  const {request} =useRequest();
 
   const registerHandler = async (email, password) => {
     
     const newUser = {email,password};
 
     //TODO REgister API call
-    const response = await fetch('http://localhost:3030/users/register',{
-      method:'POST',
-      headers:{
-        'content-type': 'application/json',
-      },
-      body: JSON.stringify(newUser),
-    });
+    const result = await request('/users/register', 'POST',newUser);
 
-    const result = await response.json();
     //Login user after register
     setUser(result);
   };
 
-  const loginHandler = (email, password) => {
-    
+  const loginHandler = async (email, password) => {
+    const result = await request('/users/login','POST',{email, password});
+    console.log(result);
+
+    setUser(result);
   }
 
   const logoutHandler = () => {
@@ -60,7 +58,7 @@ function App() {
         <Route path="/games/:gameId/edit" element={<Edit />} />
         <Route path="/games/create" element={<GameCreate />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login onLogin={loginHandler} />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout onLogout={logoutHandler} />} />
       </Routes>
 
